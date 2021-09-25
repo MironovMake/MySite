5cf0f62b9521d08972068d65048569086f4085b1
Mironov <surka2012@yandex.ru>
2021-09-25 15:56:00 +0700

    add Home page

---
+++ MyEng/script.js
@@ -0,0 +1,14 @@
+var arrLang = {
+    content: { 
+        box1: "About me", 
+        box2: "Schedule",
+        box3: "Formulas",
+        box4: "About me", 
+        box5:"Physic and prossec education",
+        box6: "Imagine my abilities.",
+        box7:"For now  be productive.", 
+        box8: "My 3d printing", 
+        box9: "asfkjbkj",
+    },
+  };
+
